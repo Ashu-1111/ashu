5578d9613432cff7893f7e41b71c619a65192ffb
Ashu_11 <118591772+Ashu-1111@users.noreply.github.com>
2023-03-21 15:56:07 +0530

    Add files via upload
    python file

---
+++ PythonP1.py
@@ -0,0 +1,11 @@
+Sub_1=int(input("Enter the marks of Sub_1: "))
+Sub_2=int(input("Enter the marks of Sub_2: "))
+Sub_3=int(input("Enter the marks of Sub_3: "))
+
+sum=Sub_1+Sub_2+Sub_3
+
+print("Total marks obtain: ",sum)
+
+avg=sum/3
+
+print("Average of the obtain marks: ",avg)
